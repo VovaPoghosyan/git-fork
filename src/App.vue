beb1f66c28e4b0e539953a8ceb42a8c07a397cfd
Vova Poghosyan <vova.poghosyan.97@gmail.com>
2020-05-06 20:50:59 +0400

    change scroll-bar style

--- src/App.vue
+++ src/App.vue
@@ -21,3 +21,20 @@ export default {
     },
 };
 </script>
+
+<style>
+    body::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 10px #ed143d94;
+        background-color: #ed143d94;
+    }
+
+    body::-webkit-scrollbar {
+        width: 10px;
+        background-color: #F5F5F5;
+    }
+
+    body::-webkit-scrollbar-thumb {
+        background-color: rgba(255, 255, 255, 0.8);
+        border-radius: 5px;
+    }
+</style>
