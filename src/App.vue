c29f56260aa6f5322dd6c1e1cc41028db8aa64b1
Vova Poghosyan <vova.poghosyan.97@gmail.com>
2020-05-06 21:48:19 +0400

    add loader

--- src/App.vue
+++ src/App.vue
@@ -37,4 +37,51 @@ export default {
         background-color: rgba(255, 255, 255, 0.8);
         border-radius: 5px;
     }
+
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        background: #00000063;
+        z-index: 10;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+
+    .lds-ripple {
+        display: inline-block;
+        position: absolute;
+        width: 80px;
+        height: 80px;
+        z-index: 11;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    .lds-ripple div {
+        position: absolute;
+        border: 4px solid #ed143d;
+        opacity: 1;
+        border-radius: 50%;
+        animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+    }
+    .lds-ripple div:nth-child(2) {
+        animation-delay: -0.5s;
+    }
+    @keyframes lds-ripple {
+        0% {
+            top: 36px;
+            left: 36px;
+            width: 0;
+            height: 0;
+            opacity: 1;
+        }
+        100% {
+            top: 0px;
+            left: 0px;
+            width: 72px;
+            height: 72px;
+            opacity: 0;
+        }
+    }
 </style>
--- src/components/ForksList.vue
+++ src/components/ForksList.vue
@@ -1,65 +1,81 @@
 <template>
     <div class="container">
         <div>
-            <md-table>
-                <md-table-row>
-                    <md-table-head>Repo Name</md-table-head>
-                    <md-table-head>Repo Owner</md-table-head>
-                    <md-table-head>Stars Count</md-table-head>
-                    <md-table-head>Forks Count</md-table-head>
-                </md-table-row>
+            <template v-if="forks_data.length">
+                <div>
+                    <md-table>
+                        <md-table-row>
+                            <md-table-head>Repo Name</md-table-head>
+                            <md-table-head>Repo Owner</md-table-head>
+                            <md-table-head>Stars Count</md-table-head>
+                            <md-table-head>Forks Count</md-table-head>
+                        </md-table-row>
 
-                <md-table-row
-                    v-for="(repo, index) in forks_data"
-                    :key="index"
-                >
-                    <md-table-cell>{{ repo.name }}</md-table-cell>
-                    <md-table-cell>{{ repo.owner.login }}</md-table-cell>
-                    <md-table-cell>{{ repo.stargazers_count }}</md-table-cell>
-                    <md-table-cell>{{ repo.forks_count }}</md-table-cell>
-                </md-table-row>
-            </md-table>
-            <div class="pagination">
-                <md-button
-                    :disabled="current_page === 1"
-                    class="md-icon-button md-raised"
-                    @click="changePage(--current_page)"
-                >
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        viewBox="0 0 24 24" 
-                        fill="black" 
-                        width="18px" 
-                        height="18px"
-                    >
-                        <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
-                        <path 
-                            d="M0 0h24v24H0z" 
-                            fill="none"/>
-                    </svg>
-                </md-button>
-                <md-button class="md-icon-button md-raised">
-                    {{ current_page }}
-                </md-button>
-                <md-button
-                    :disabled="current_page === pages_count"
-                    class="md-icon-button md-raised"
-                    @click="changePage(++current_page)"
-                >
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        viewBox="0 0 24 24" 
-                        fill="black" 
-                        width="18px" 
-                        height="18px"
-                    >
-                        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
-                        <path 
-                            d="M0 0h24v24H0z" 
-                            fill="none"/>
-                    </svg>
-                </md-button>
-            </div>
+                        <md-table-row
+                            v-for="(repo, index) in forks_data"
+                            :key="index"
+                        >
+                            <md-table-cell>{{ repo.name }}</md-table-cell>
+                            <md-table-cell>{{ repo.owner.login }}</md-table-cell>
+                            <md-table-cell>{{ repo.stargazers_count }}</md-table-cell>
+                            <md-table-cell>{{ repo.forks_count }}</md-table-cell>
+                        </md-table-row>
+                    </md-table>
+                    <div class="pagination">
+                        <md-button
+                            :disabled="current_page === 1"
+                            class="md-icon-button md-raised"
+                            @click="changePage(--current_page)"
+                        >
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 24 24" 
+                                fill="black" 
+                                width="18px" 
+                                height="18px"
+                            >
+                                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
+                                <path 
+                                    d="M0 0h24v24H0z" 
+                                    fill="none"/>
+                            </svg>
+                        </md-button>
+                        <md-button class="md-icon-button md-raised">
+                            {{ current_page }}
+                        </md-button>
+                        <md-button
+                            :disabled="current_page === pages_count"
+                            class="md-icon-button md-raised"
+                            @click="changePage(++current_page)"
+                        >
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 24 24" 
+                                fill="black" 
+                                width="18px" 
+                                height="18px"
+                            >
+                                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+                                <path 
+                                    d="M0 0h24v24H0z" 
+                                    fill="none"/>
+                            </svg>
+                        </md-button>
+                    </div>
+                </div>
+            </template>
+            <template v-else>
+                <div >
+                    
+                </div>
+            </template>
+            <template v-if="loading">
+                <div class="backdrop"></div>
+                <div class="lds-ripple">
+                    <div></div>
+                    <div></div>
+                </div>
+            </template>
         </div>
 
     </div>
@@ -76,6 +92,7 @@ export default {
             per_page: 10,
             forks_data: [],
             repo_info: {},
+            loading: true,
         };
     },
     mounted() {
@@ -110,13 +127,16 @@ export default {
     },
     methods: {
         getForksData(){
+            this.loading = true;
             let userName = this.repo_info.owner;
             let repoName = this.repo_info.repo_name;
             axios.get(`http://localhost:8080/repos/${userName}/${repoName}/forks?page=${this.current_page}&per_page=${this.per_page}`)
                 .then(response => {
                     console.log('////////////',  response.data);
                     this.forks_data = response.data;
+                    this.loading = false;
                 }).catch(error => {
+                    this.loading = false;
                     console.log(error);
                 });
         },
